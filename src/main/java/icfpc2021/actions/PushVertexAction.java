16b3ff58815817907cb733927db9413cde3792ca
Vadim Tsesko <incubos@users.noreply.github.com>
2021-07-09 21:33:04 +0300

    WIP: PushVertexAction

--- src/main/java/icfpc2021/actions/PushVertexAction.java
+++ src/main/java/icfpc2021/actions/PushVertexAction.java
@@ -4,8 +4,8 @@ import icfpc2021.model.Edge;
 import icfpc2021.model.Figure;
 import icfpc2021.model.Vertex;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
 
 public class PushVertexAction implements Action {
     public int vertex;
@@ -16,24 +16,6 @@ public class PushVertexAction implements Action {
         this.vertex = vertex;
         this.dX = dX;
         this.dY = dY;
-    }
-
-    @Override
-    public Figure apply(Figure figure) {
-        // Target invariant
-        long[] targetEdgeSquareLengths = squareEdgeLengthsFrom(figure.vertices, figure.edges);
-
-        // Allocate mutable vertices and replace the touched vertex
-        List<Vertex> vertices = new ArrayList<>(figure.vertices);
-        vertices.set(vertex, figure.vertices.get(vertex).move(dX, dY));
-
-        // TODO: Try to move adjacent vertices until the target is reached
-        //???
-
-        // TODO: Stretch the graph
-        // TODO: Keep away from the hole border
-
-        return new Figure(vertices, figure.edges);
     }
 
     private static long[] squareEdgeLengthsFrom(
@@ -49,5 +31,69 @@ public class PushVertexAction implements Action {
             result[i] = dx * dx + dy * dy;
         }
         return result;
+    }
+
+    @Override
+    public Figure apply(Figure figure) {
+        if (dX == 0 && dY == 0) {
+            // Nothing changed
+            return figure;
+        }
+
+        // Calculate target invariant
+        final long[] targetEdgeSquareLengths = squareEdgeLengthsFrom(figure.vertices, figure.edges);
+
+        // Allocate mutable vertices and replace the touched vertex
+        final List<Vertex> vertices = new ArrayList<>(figure.vertices);
+        // Now the target is violated
+        vertices.set(vertex, figure.vertices.get(vertex).move(dX, dY));
+
+        // Try BFS traversal and move the vertices until the target is reached
+        bfsExceptStart(figure.edges, vertex, integer -> {
+            throw new UnsupportedOperationException("Implement me!");
+
+        });
+
+        // TODO: Keep away from the hole border
+
+        return new Figure(vertices, figure.edges);
+    }
+
+    private void bfsExceptStart(
+            List<Edge> edges,
+            int startVertex,
+            Function<Integer, Boolean> processVertexAndContinue) {
+        Set<Integer> visited = new HashSet<>();
+        // Do not process start vertex
+        visited.add(startVertex);
+
+        Deque<Integer> queue = new LinkedList<>();
+        queue.add(startVertex);
+        while (!queue.isEmpty()) {
+            int vertex = queue.removeFirst();
+
+            // Expand neighbours
+            edges.forEach(edge -> {
+                if (edge.start == vertex) {
+                    queue.addLast(edge.end);
+                } else if (edge.end == vertex) {
+                    queue.addLast(edge.start);
+                }
+            });
+
+            // Skip visited
+            if (visited.contains(vertex)) {
+                continue;
+            }
+
+            // Process
+            if (processVertexAndContinue.apply(vertex)) {
+                // And mark visited
+                visited.add(vertex);
+            } else {
+                // The goal is reached
+                return;
+            }
+        }
     }
 }
