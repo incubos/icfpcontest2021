d05d6589b026a5201cf3a4f8a151469cf1ab859c
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2021-07-10 14:13:56 +0300

    Local solver with printing results to solutions

--- src/main/java/icfpc2021/LocalSolver.java
+++ src/main/java/icfpc2021/LocalSolver.java
@@ -1,22 +1,35 @@
 package icfpc2021;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import icfpc2021.model.Figure;
 import icfpc2021.model.LambdaMan;
+import icfpc2021.model.Pose;
 import icfpc2021.model.Task;
 import icfpc2021.strategy.AutoKutuzoffStrategy;
 import icfpc2021.viz.State;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Set;
 
 public class LocalSolver {
+
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    // Ignore for now
+    private static Set<Integer> IGNORED = Set.of(7);
 
     public static void main(String[] args) throws IOException {
         int correctValues = 0;
         int fitted = 0;
         int solved = 0;
-        for (int i = 1; i < 78; i++) {
+        for (int i = 1; i < 10; i++) {
             System.out.println("Task " + i);
+            if (IGNORED.contains(i)) {
+                System.out.println("IGNORED");
+                continue;
+            }
             String taskName = String.format("%03d.json", i);
             Path problemPath = Path.of("problems", taskName);
             var task = Task.fromJsonFile(problemPath);
@@ -41,6 +54,10 @@ public class LocalSolver {
             }
             if (correct && fits) {
                 solved += 1;
+                Pose pose = Pose.fromVertices(state.getMan().figure.vertices);
+                String json = objectMapper.writeValueAsString(pose);
+                Path path = Path.of("solutions", taskName);
+                Files.writeString(path, json);
             }
             System.out.println("Correct " + correct + "; Fits " + fits);
         }
