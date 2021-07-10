819a24ae0883db0259688f5efd5232ea0f0f4b37
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2021-07-10 12:58:00 +0300

    Slight modifications for Auto actions

--- src/main/java/icfpc2021/actions/AutoCenterAction.java
+++ src/main/java/icfpc2021/actions/AutoCenterAction.java
@@ -1,5 +1,6 @@
 package icfpc2021.actions;
 
+import icfpc2021.ScoringUtils;
 import icfpc2021.model.Figure;
 import icfpc2021.model.Vertex;
 import icfpc2021.viz.State;
@@ -10,13 +11,29 @@ import static icfpc2021.ConvexHullKt.convexHull;
 
 public class AutoCenterAction implements Action {
 
+    int slightlyMoveDelta = 3;
+
     @Override
     public Figure apply(State state, Figure figure) {
         var figureCenterCoords = figureCenter(convexHull(figure.vertices));
         var holeCenterCoords = figureCenter(state.getHoleConvexHull());
         var dx = holeCenterCoords.x - figureCenterCoords.x;
         var dy = holeCenterCoords.y - figureCenterCoords.y;
-        return new MoveAction(dx, dy).apply(state, figure);
+        MoveAction moveAction = new MoveAction(dx, dy);
+        Figure movedFigure = moveAction.apply(state, figure);
+        if (ScoringUtils.fitsWithinHole(figure, state.getHole())) {
+            return movedFigure;
+        }
+        // Slightly move to fit if we can
+        for (int x = -slightlyMoveDelta; x <= slightlyMoveDelta; x++) {
+            for (int y = -slightlyMoveDelta; y <= slightlyMoveDelta; y++) {
+                Figure slightlyMoved = new MoveAction(x, y).apply(state, movedFigure);
+                if (ScoringUtils.fitsWithinHole(slightlyMoved, state.getHole())) {
+                    return slightlyMoved;
+                }
+            }
+        }
+        return movedFigure;
     }
 
     public static Vertex figureCenter(List<Vertex> vertices) {
--- src/main/java/icfpc2021/actions/AutoFoldAction.java
+++ src/main/java/icfpc2021/actions/AutoFoldAction.java
@@ -1,7 +1,12 @@
 package icfpc2021.actions;
 
 import icfpc2021.model.Figure;
+import icfpc2021.model.Vertex;
 import icfpc2021.viz.State;
+import org.apache.commons.math3.util.Pair;
+
+import java.util.HashSet;
+import java.util.List;
 
 import static icfpc2021.ConvexHullKt.area;
 import static icfpc2021.ConvexHullKt.convexHull;
@@ -17,12 +22,20 @@ public class AutoFoldAction implements Action {
     @Override
     public Figure apply(State state, Figure figure) {
         // We try to minimize convex hull area with fold
+        List<Vertex> convexHull = convexHull(figure.vertices);
+        var convexHullEdges = new HashSet<Pair<Integer, Integer>>();
+        for (int i = 0; i < convexHull.size(); i++) {
+            convexHullEdges.add(
+                    Pair.create(figure.vertices.indexOf(convexHull.get(i)),
+                            figure.vertices.indexOf(convexHull.get(i + 1 % convexHull.size())))
+            );
+        }
         var minArea = 10e10;
         FoldAction minFoldAction = null;
         for (int i = 0; i < figure.vertices.size(); i++) {
             for (int j = i + 1; j < figure.vertices.size(); j++) {
                 for (int k = 0; k < figure.vertices.size(); k++) {
-                    if (k != i && k != j) {
+                    if (!convexHullEdges.contains(Pair.create(i, j)) && k != i && k != j) {
                         FoldAction fa = new FoldAction(i, j, k);
                         var newFigure = fa.apply(state, figure);
                         if (checkFigure(state.getOriginalMan().figure, newFigure, state.getOriginalMan().epsilon)) {
