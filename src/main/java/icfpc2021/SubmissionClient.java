53958e24ce68b3feaefe5fe4b8c0b531b7ebc93e
Anton Volokhov <avolokhov@apple.com>
2021-07-10 10:22:50 +0100

    submission client

---
+++ src/main/java/icfpc2021/SubmissionClient.java
@@ -0,0 +1,66 @@
+package icfpc2021;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class SubmissionClient {
+    private static final Logger log = LoggerFactory.getLogger(PosesHttpClient.class);
+
+    public void submit(String pose) {
+        var serverUrl = "https://poses.live/api";
+        var apiToken = "f1b30d61-a94e-4980-bd2d-66f8b9dd8714";
+        var problemsCount = 78;
+
+        try {
+            Files.createDirectory(Path.of("problems"));
+        } catch (IOException ignored) {
+        }
+
+        for (int i = 0; i < problemsCount; i++) {
+            try {
+                var request = HttpRequest.newBuilder()
+                        .uri(URI.create(String.format("%s/problems/%d/solutions", serverUrl, i + 1)))
+                        .version(HttpClient.Version.HTTP_1_1)
+                        .header("Authorization", "Bearer " + apiToken)
+                        .POST(HttpRequest.BodyPublishers.ofString(pose))
+                        .build();
+
+                var response = HttpClient.newHttpClient()
+                        .send(request, HttpResponse.BodyHandlers.ofString());
+
+                var status = response.statusCode();
+
+                if (status != HttpURLConnection.HTTP_OK) {
+                    log.error("Unexpected server response:");
+                    log.error("HTTP code: {}", status);
+                    log.error("Response body: {}", response.body());
+                }
+            } catch (Exception e) {
+                log.error("Unexpected server response", e);
+                System.exit(1);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Files.list(Paths.get(args[0]))
+                .map(o -> {
+                    try {
+                        return Files.readString(o);
+                    } catch (IOException e) {
+                        throw new RuntimeException("boom!");
+                    }
+                })
+                .forEach(new SubmissionClient()::submit);
+    }
+}
